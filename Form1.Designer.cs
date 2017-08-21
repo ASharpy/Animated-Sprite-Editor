47390555044fa6c1cf754e67e7b6b6e35ab19099
ASharpy <a.sharp3197@gmail.com>
2017-08-21 14:59:24 +1000

    Able to insert picture and draw a rectangle working on getting a select button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.AddToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SpriteSheet = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Select = new System.Windows.Forms.CheckBox();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpriteSheet)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuBar;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.MdiWindowListItem = this.fileToolStripMenuItem;
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1240, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1354, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,43 +69,82 @@
             // AddToolStripMenuItem
             // 
             this.AddToolStripMenuItem.Name = "AddToolStripMenuItem";
-            this.AddToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.AddToolStripMenuItem.Text = "Add";
+            this.AddToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.AddToolStripMenuItem.Text = "Add Image";
+            this.AddToolStripMenuItem.Click += new System.EventHandler(this.AddToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.saveToolStripMenuItem.Text = "Save Image";
             // 
             // removeToolStripMenuItem
             // 
             this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
-            this.removeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.removeToolStripMenuItem.Text = "Remove Image";
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(600, 130);
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 2;
+            this.panel1.Size = new System.Drawing.Size(175, 756);
+            this.panel1.TabIndex = 4;
             // 
-            // button1
+            // panel2
             // 
-            this.button1.Location = new System.Drawing.Point(154, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.Select);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(175, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1179, 99);
+            this.panel2.TabIndex = 5;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // SpriteSheet
+            // 
+            this.SpriteSheet.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.SpriteSheet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SpriteSheet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SpriteSheet.Location = new System.Drawing.Point(175, 123);
+            this.SpriteSheet.Name = "SpriteSheet";
+            this.SpriteSheet.Size = new System.Drawing.Size(1179, 657);
+            this.SpriteSheet.TabIndex = 6;
+            this.SpriteSheet.TabStop = false;
+            this.SpriteSheet.Click += new System.EventHandler(this.SpriteSheet_Click);
+            this.SpriteSheet.Paint += new System.Windows.Forms.PaintEventHandler(this.SpriteSheet_Paint);
+            this.SpriteSheet.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SpriteSheet_MouseDown);
+            this.SpriteSheet.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SpriteSheet_MouseMove);
+            this.SpriteSheet.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SpriteSheet_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Select
+            // 
+            this.Select.AutoSize = true;
+            this.Select.Location = new System.Drawing.Point(82, 36);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(80, 17);
+            this.Select.TabIndex = 0;
+            this.Select.Text = "checkBox1";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.CheckedChanged += new System.EventHandler(this.Select_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1240, 651);
+            this.ClientSize = new System.Drawing.Size(1354, 780);
+            this.Controls.Add(this.SpriteSheet);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
@@ -109,7 +154,9 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpriteSheet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,7 +170,10 @@
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox SpriteSheet;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox Select;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,98 @@ namespace Animated_Sprite_Editor
 {
     public partial class Form1 : Form
     {
+        private Point startPoint = new Point();
+        private Point endPoint = new Point();
+
+
         public Form1()
         {
             InitializeComponent();
+            SpriteSheet.Paint += new System.Windows.Forms.PaintEventHandler(SpriteSheet_Paint);
+            SpriteSheet.MouseDown += new System.Windows.Forms.MouseEventHandler(SpriteSheet_MouseDown);
         }
 
+        private void AddSpriteSheet()
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Sprite Sheet";
+                dlg.Filter = "Image Files(*.BMP;*.JPG;*.GIF,*.PNG;)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    SpriteSheet.Image = Image.FromFile(dlg.FileName);
+                    SpriteSheet.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                }
+            }
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddSpriteSheet();
+        }
+
+        private void SpriteSheet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SpriteSheet_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPoint = e.Location;
+            }
+        }
+
+        private void SpriteSheet_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                endPoint = e.Location;
+            }
+          
+
+        }
+
+        private void SpriteSheet_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                endPoint = e.Location;
+            }
+           
+        }
+
+        private void SpriteSheet_Paint(object sender, PaintEventArgs e)
+        {
+            if (startPoint.X > 0 && startPoint.Y > 0 && endPoint.X > 0 && endPoint.Y > 0)
+            {
+
+
+                e.Graphics.DrawRectangle(Pens.Blue, new Rectangle(startPoint.X, startPoint.Y, endPoint.X - startPoint.X, endPoint.Y - startPoint.Y));
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SpriteSheet.Refresh();
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Select_CheckedChanged(object sender, EventArgs e)
         {
 
         }
